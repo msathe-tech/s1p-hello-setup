4390b8e98b7c8b53a980cd9c249f691bef906bdf
msathe-tech <msathe@Madhavs-MacBook-Pro.local>
2019-08-24 15:48:41 -0400

    clean up pipelines

--- ci/fly-set-pipeline.sh
+++ ci/fly-set-pipeline.sh
@@ -4,8 +4,15 @@ echo "Enter project name [s1p-hello-service]: "
 read project_name
 project_name=${project_name:-s1p-hello-service}
 
-fly -t w sp -p ${project_name} \
+echo "Do you want to repo if it exists (y/n) [y]: "
+read delete
+
+if [${delete} == "y"]; then
+	fly -t w dp -p ${project_name}
+fi
+
+echo "fly -t w sp -p ${project_name} \
 	--config ci/s1p-pipeline.yml \
 	-l ci/config/s1p-common-config.yml \
 	-l ci/creds/s1p-common-creds.yml \
-	-l ci/config/${project_name}-config.yml
+	-l ci/config/${project_name}-config.yml"
