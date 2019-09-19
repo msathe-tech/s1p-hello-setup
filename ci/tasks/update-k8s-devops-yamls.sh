9bbb0c471756ae57791d12f0b6c026d5caf663a8
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-19 05:25:31 -0400

    updated

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -6,7 +6,7 @@ cd devops-repo-modified
 # For now, hard-coding baseline sha as 64414b
 # In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
 cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > spinnaker-prod-baseline.yaml
-echo "#" >> spinnaker-prod-baseline.yaml
+echo "" >> spinnaker-prod-baseline.yaml
 git add .
 git config --global user.email "s1p-concourse@no.op"
 git config --global user.name "S1P Concourse"
