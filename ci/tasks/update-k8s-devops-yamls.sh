4b72bd270c15e0cc93610c89d3b0f2fa89355cae
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-19 05:12:07 -0400

    should be working...

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
-#export ROOT_FOLDER
-#ROOT_FOLDER="$( pwd )"
-#
-#source "${ROOT_FOLDER}/ci/tasks/resource-utils.sh"
-#echo "Loading git key to enable push"
-#export TMPDIR=/tmp
-#echo "${GIT_PRIVATE_KEY}" > "${TMPDIR}/git-resource-private-key"
-#load_pubkey
 
-echo "Current dir:"
-pwd
 git clone devops-repo devops-repo-modified
 cd devops-repo-modified
+# For now, hard-coding baseline sha as 64414b
+# In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
 cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > spinnaker-prod-baseline.yaml
 echo "" >> spinnaker-prod-baseline.yaml
 git add .
 git config --global user.email "s1p-concourse@no.op"
 git config --global user.name "S1P Concourse"
 git commit -m "Updated baseline tag with SHA"
-
-
-# For now, hard-coding baseline sha as 64414b
-# In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
