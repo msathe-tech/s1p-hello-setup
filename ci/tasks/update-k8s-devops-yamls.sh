3e71fa8f7352f73173c6298c4198388cdfe68230
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-19 04:15:19 -0400

    do not change directions

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env bash
+##!/usr/bin/env bash
+#export ROOT_FOLDER
+#ROOT_FOLDER="$( pwd )"
+#
+#source "${ROOT_FOLDER}/ci/tasks/resource-utils.sh"
+#echo "Loading git key to enable push"
+#export TMPDIR=/tmp
+#echo "${GIT_PRIVATE_KEY}" > "${TMPDIR}/git-resource-private-key"
+#load_pubkey
+
 # For now, hard-coding baseline sha as 64414b
 # In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
-cd devops-repo
-cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > spinnaker-prod-baseline.yaml
-echo "" >> spinnaker-prod-baseline.yaml
+cat devops-repo/spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > devops-repo/spinnaker-prod-baseline.yaml
+echo "" >> devops-repo/spinnaker-prod-baseline.yaml
