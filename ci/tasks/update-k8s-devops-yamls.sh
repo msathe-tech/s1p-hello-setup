ddbadb80ccd14b9fdf2ad470790581b6282c4625
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-19 04:27:15 -0400

    git add and commit in task

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -8,7 +8,12 @@
 #echo "${GIT_PRIVATE_KEY}" > "${TMPDIR}/git-resource-private-key"
 #load_pubkey
 
+git clone devops-repo devops-repo-modified
+cd devops-repo-modified
+cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > spinnaker-prod-baseline.yaml
+echo "" >> spinnaker-prod-baseline.yaml
+git add .
+git commit -m "Updated baseline tag with SHA"
+
 # For now, hard-coding baseline sha as 64414b
 # In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
-cat devops-repo/spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414b/g' > devops-repo/spinnaker-prod-baseline.yaml
-echo "" >> devops-repo/spinnaker-prod-baseline.yaml
