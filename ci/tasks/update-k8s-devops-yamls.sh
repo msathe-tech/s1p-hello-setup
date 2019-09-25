9babfac1a3bd7db7a9e47a47fc5172159c4f437d
Madhav Sathe <msathe-tech@gmail.com>
2019-09-24 23:18:28 -0400

    add bump file to bump git repo

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -7,7 +7,7 @@ cd devops-repo-modified
 # In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
 cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414bd/g' > spinnaker-prod-baseline.yaml
 echo -e "\n#" >> spinnaker-prod-baseline.yaml
-git add .
+date > bump
 git config --global user.email "s1p-concourse@no.op"
 git config --global user.name "S1P Concourse"
 git commit -m "Updated baseline tag with SHA"
