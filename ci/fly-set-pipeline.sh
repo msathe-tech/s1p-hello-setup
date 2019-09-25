7d1a5580a832fe2428df9f6f362d1ff6d90e64b2
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-25 06:20:21 -0400

    cleanup

--- ci/fly-set-pipeline.sh
+++ ci/fly-set-pipeline.sh
@@ -7,7 +7,7 @@ project_name=${project_name:-s1p-hello-service}
 echo "Do you want to delete the pipeline if it exists (y/n) [y]: "
 read delete
 
-if [ ${delete} == "y" ]; then
+if [ ${delete} = "y" ]; then
 	fly -t w dp -p ${project_name}
 fi
 
@@ -15,4 +15,5 @@ fly -t w sp -p ${project_name} \
 	--config ci/s1p-pipeline.yml \
 	-l ci/config/s1p-common-config.yml \
 	-l ci/creds/s1p-common-creds.yml \
-	-l ci/config/${project_name}-config.yml
+	-l ci/config/${project_name}-config.yml \
+	--var "github-private-key=$(cat ~/.ssh/id_rsa)"
