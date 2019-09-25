b38c084274b9546fba2d3202dc16e27e20d8d551
Cora Iberkleid <ciberkleid@pivotal.io>
2019-09-25 04:34:53 -0400

    set shae from code-repo in deployment yamls

--- ci/tasks/update-k8s-devops-yamls.sh
+++ ci/tasks/update-k8s-devops-yamls.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+echo "Get latest code-repo sha"
+cd code-repo
+sha=`git rev-parse --short HEAD`
+cd ..
 echo "Cloning devops-repo"
 git clone devops-repo devops-repo-modified
 cd devops-repo-modified
 # For now, hard-coding baseline sha as 64414b
 # In the future, cd to code-repo, get the sha or tag of last prod deployment, and use that in the replacement below
-cat spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414bd/g' > spinnaker-prod-baseline.yaml
+cat templates/spinnaker-prod-baseline-template.yaml | sed 's/BASELINE-SHA/64414bd/g' > spinnaker-prod-baseline.yaml
 echo -e "\n#" >> spinnaker-prod-baseline.yaml
+cat templates/spinnaker-prod-canary-template.yaml | sed "s/GIT-SHA/${sha}/g" > spinnaker-prod-canary.yaml
+cat templates/spinnaker-prod-template.yaml | sed "s/GIT-SHA/${sha}/g" > spinnaker-prod.yaml
 date > bump
 git add .
 git config --global user.email "s1p-concourse@no.op"
